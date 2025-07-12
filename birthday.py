f4d2770e128cbf1508839c6df53f2dbdcd335231
umar10030 <saiyedzubair@gmail.com>
2025-07-12 10:55:41 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,26 @@
+birthday1 = " vihan January 1"
+birthday2 = "kelab February 14"
+birthday3 = "prathah March 3"
+birthday4 = "hamza April 25"
+birthday5 = "omar May 5"
+birthday6 = "prithvi June 15"
+birthday7 = "rajesh July 20"
+birthday8 = "evaan August 30"
+birthday9 = "evan September 10"
+birthday10 = "raj October 31"
+birthday11 = "atharv November 11"
+birthday12 = "abdulah December 25"
+
+print("List of birthdays:")
+print(birthday1)
+print(birthday2)
+print(birthday3)
+print(birthday4)
+print(birthday5)
+print(birthday6)
+print(birthday7)
+print(birthday8)
+print(birthday9)
+print(birthday10)
+print(birthday11)
+print(birthday12)
